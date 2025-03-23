f8b1152fcf9af90a1e0eadc66234b1590d906c4c
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-03-24 00:54:22 +0600

    Create board private variable

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const gameBoard = (function () {
+  const N = 3;
+
+  const board = [];
+  for (let i = 0; i < N; ++i) {
+    board[i] = [];
+    for (let j = 0; j < N; ++j)
+      board[i].push(null);
+  }
+
   return {
 
   };
